bb3aee6e91f3247e6600fe7bf947b54352c3b3f8
wlrmynt <wmi54968@gmail.com>
2025-12-16 13:25:26 +0700

    Setup project configuration and splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:celoe_lms/screens/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to Login Screen after 3 seconds
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image.asset(
+          'assets/Splash Screen.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
